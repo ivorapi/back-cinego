c199eb2b81dfb6575c09434cae50fa787c961512
sanmart22 <francosanmartino22@gmail.com>
2026-08-30 18:52:57 -0300

    agregar repositorios de Asiento y Entrada

--- src/main/java/com/uade/demo/repository/UsuarioRepository.java
+++ src/main/java/com/uade/demo/repository/UsuarioRepository.java
@@ -1,13 +1,17 @@
 package com.uade.demo.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.uade.demo.model.Usuarios;
 
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuarios, Long> {
     // public List<UsuarioModel> findAll(){
     //     String sql = "SELECT * FROM usuarios";
     //     return null;
     // }
-    
+    Optional<Usuarios> findByEmail(String email);
 }
